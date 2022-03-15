4359dcc78dd177cc6c8d3377e05d2f377f054956
KentaGoto <gotokentag@gmail.com>
2022-03-15 10:56:43 +0900

    ソースとターゲットのみ

--- sdltm-export.pl
+++ sdltm-export.pl
@@ -1,23 +1,32 @@
 use strict;
-#use warnings;
+use warnings;
 use utf8;
 use DBI;
 
 my $dbh = DBI->connect("dbi:SQLite:dbname=test.sdltm");
-my $select = "select * from translation_units;";
+
+# ソースとターゲットのみ対象とする
+my $select = "select source_segment, target_segment from translation_units;"; 
+
 my $sth = $dbh->prepare($select);
 $sth->execute;
 
 open my $out, '>', 'result.txt' or die;
 
-while(my @row = $sth->fetchrow_array){
-	#print {$out} join("\t", @row), "\n";
-	print {$out} join(", ", @row), "\n";
+while(my ($source, $target) = $sth->fetchrow()){
+	$source = &seikei($source);
+	$target = &seikei($target);
+	print {$out} $source . "\t" . $target . "\n";
 }
-
-#$sth->finish;
-#undef $sth;
 
 $dbh->disconnect;
 
 close $out;
+print "Done!\n";
+
+sub seikei {
+	my $s = shift;
+	$s =~ s{^.+<Value>(.+)</Value>.+$}{$1};
+
+	return $s;
+}
